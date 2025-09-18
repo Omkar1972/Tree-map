e2c351d95a5fe29fd59312cfb39ac06ea869597a
Omkar1972 <omkarbagmare49@gmail.com>
2025-09-18 15:47:48 +0530

    sixth commit

---
+++ T17.java
@@ -0,0 +1,31 @@
+import java.util.TreeMap;
+
+public class T17 {
+
+	public static void main(String[] args) {
+	 TreeMap<Integer,String> tm=new TreeMap<Integer,String>();
+	 
+	 tm.put(10, "Red");
+	 tm.put(20, "Green");
+	 tm.put(40, "Black");
+	 tm.put(50, "White");
+	 tm.put(60, "Pink");
+	 
+	 System.out.println("Value before poll: "+tm);
+	 
+	 System.out.println();
+	 
+	 System.out.println("Value returned: "+tm.pollFirstEntry());
+	 
+	 System.out.println("Value after poll: "+tm);
+
+	}
+
+}
+//Test input:
+//{10=Red, 20=Green, 40=Black, 50=White, 60=Pink}
+//
+//
+//Sample Output:Value before poll: {10=Red, 20=Green, 40=Black, 50=White, 60=Pink}
+//Value returned: 10=Red 
+//Value after poll: {20=Green, 40=Black, 50=White, 60=Pink}
